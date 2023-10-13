64011060d3456c1761cace17bfd07d8c8f87a3b2
alejandroem <alexalejandroem@gmail.com>
2023-10-13 09:51:32 -0300

    Add: Finished master tool for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
-
 import 'domain/printer.dart';
 import 'infrastructure/epson_network_printer.dart';
 import 'infrastructure/epson_serial_printer.dart';
@@ -16,24 +14,61 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  //String ipAddress = '192.168.100.1';
   String ipAddress = "192.168.100.74";
   String comPort = 'COM3';
   List<String> logs = [];
+  String printText = '';
+  int feedLines = 1;
 
   Printer? serialPrinter;
   Printer? networkPrinter;
 
+  bool isNetworkConnection = true;
+
   @override
   void initState() {
     super.initState();
-
     serialPrinter = EpsonSerialPrinter();
     networkPrinter = EpsonNetworkPrinter();
   }
 
+  void logAction(String message) {
+    setState(() {
+      logs.add(message);
+    });
+    log(message);
+  }
+
+  Future<void> executePrinterCommand(Function action) async {
+    Printer? activePrinter =
+        isNetworkConnection ? networkPrinter : serialPrinter;
+    await activePrinter!.connection
+        .connect(isNetworkConnection ? ipAddress : comPort);
+    await action();
+    await activePrinter.connection.disconnect();
+  }
+
+  Future<void> openDrawerAndWait(Printer? printer) async {
+    await executePrinterCommand(() async {
+      await printer!.kickDrawer(2);
+      int counter = 0;
+      do {
+        await Future.delayed(const Duration(seconds: 1));
+        counter++;
+        if (counter > 10) {
+          logAction('Drawer did not open after 10 seconds via');
+          break;
+        }
+        logAction('Waiting for drawer to close');
+      } while (await printer.isDrawerOpen());
+      logAction('Closed drawer via $serialPrinter');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    String activeConnectionType = isNetworkConnection ? "Network" : "Serial";
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -44,81 +79,166 @@ class _MyAppState extends State<MyApp> {
           title: const Text('ESC/POS Printing Demo'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter IP Address',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    ipAddress = value;
-                  });
-                },
+          child: Stack(
+            children: [
+              Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter IP Address',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        ipAddress = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter COM Port',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        comPort = value;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    onChanged: (value) {
+                      setState(() {
+                        printText = value;
+                      });
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Text to print',
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    onChanged: (value) {
+                      setState(() {
+                        feedLines = int.tryParse(value) ?? 1;
+                      });
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Number of lines to feed',
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 16),
+                  SwitchListTile(
+                    title: const Text('Connection Type'),
+                    value: isNetworkConnection,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isNetworkConnection = value;
+                      });
+                    },
+                    secondary: const Icon(Icons.swap_horiz),
+                    subtitle: Text('Currently using: $activeConnectionType'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await executePrinterCommand(() async {
+                        if (isNetworkConnection) {
+                          await networkPrinter!.print(printText);
+                        } else {
+                          await serialPrinter!.print(printText);
+                        }
+                      });
+                      logAction('Printed text via $activeConnectionType');
+                    },
+                    child: Text('Print Text via $activeConnectionType'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await executePrinterCommand(() async {
+                        if (isNetworkConnection) {
+                          await networkPrinter!.feed(feedLines);
+                        } else {
+                          await serialPrinter!.feed(feedLines);
+                        }
+                      });
+                      logAction('Fed lines via $activeConnectionType');
+                    },
+                    child: Text('Feed Lines via $activeConnectionType'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await executePrinterCommand(() async {
+                        if (isNetworkConnection) {
+                          await networkPrinter!.cut();
+                        } else {
+                          await serialPrinter!.cut();
+                        }
+                      });
+                      logAction('Cut paper via $activeConnectionType');
+                    },
+                    child: Text('Cut Paper via $activeConnectionType'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await executePrinterCommand(() async {
+                        if (isNetworkConnection) {
+                          await networkPrinter!.kickDrawer(2);
+                        } else {
+                          await serialPrinter!.kickDrawer(2);
+                        }
+                      });
+                    },
+                    child: const Text('Kick Drawer'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await executePrinterCommand(() async {
+                        if (isNetworkConnection) {
+                          bool open = await networkPrinter!.isDrawerOpen();
+                          logAction(
+                              'Drawer status: ' + (open ? 'Open' : 'Closed'));
+                        } else {
+                          bool open = await serialPrinter!.isDrawerOpen();
+                          logAction(
+                              'Drawer status: ' + (open ? 'Open' : 'Closed'));
+                        }
+                      });
+                    },
+                    child: const Text('Check Drawer Status'),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (isNetworkConnection) {
+                        await openDrawerAndWait(networkPrinter);
+                      } else {
+                        await openDrawerAndWait(serialPrinter);
+                      }
+                    },
+                    child:
+                        Text('Open Drawer and Wait via $activeConnectionType'),
+                  ),
+                  const SizedBox(height: 16),
+                ],
               ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter COM Port',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    comPort = value;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  networkPrinter!.connection.connect(ipAddress);
-                  networkPrinter!.println(
-                      'Hellosssssssssssssssssssssssssssssssssssssssssssssssswwwwwwwwwwwwwwwsssssssss World!');
-                  networkPrinter!.feed(2);
-                  networkPrinter!.cut();
-                  networkPrinter!.kickDrawer(2);
-                  networkPrinter!.connection.disconnect();
-                },
-                child: const Text('Print via IP'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  serialPrinter!.connection.connect(comPort);
-                  bool open = await serialPrinter!.isDrawerOpen();
-                  log(open.toString());
-                  do {
-                    await Future.delayed(const Duration(seconds: 1));
-                    log("Drawer is open close it");
-                  } while (await serialPrinter!.isDrawerOpen());
-                  log("Drawer is closed");
-
-                  serialPrinter!.connection.disconnect();
-                },
-                child: const Text('Drawer Status'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  serialPrinter!.connection.connect(comPort);
-                  serialPrinter!.println(
-                      'Hello Worldsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss!1');
-                  serialPrinter!.kickDrawer(2);
-                  serialPrinter!.feed(10);
-                  serialPrinter!.cut();
-                  serialPrinter!.printBarcode(123123123123123);
-                  bool status = await networkPrinter!.isDrawerOpen();
-                  log(status.toString());
-                  serialPrinter!.connection.disconnect();
-                },
-                child: const Text('Print via COM'),
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: logs.length,
-                  itemBuilder: (context, index) {
-                    return Text(logs[index]);
-                  },
+              Positioned(
+                top: 0,
+                right: 0,
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: logs.length,
+                    itemBuilder: (context, index) {
+                      return Text(logs[index]);
+                    },
+                  ),
                 ),
               ),
             ],
